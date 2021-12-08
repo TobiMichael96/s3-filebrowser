4472d33a109dd90325b60ddc0dcf5aae4a724e4c
Tobias Effner <tobias.effner@valtech-mobility.com>
2021-12-08 10:37:30 +0100

    ADD DISABLE AUTH

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ if [ -z "${AWS_S3_BUCKET}" ]; then
     exit
 fi
 
+# Check variables
+if [ "${DISABLE_AUTH}" ]; then
+    echo "You disabled authentication!"
+    additional_flag="--auth.method='none'"
+fi
+
 echo $AWS_ACCESS_KEY:$AWS_SECRET_KEY > /root/.passwd-s3fs
 chmod 600 /root/.passwd-s3fs
 
@@ -19,4 +25,4 @@ mkdir /data
 
 s3fs $AWS_S3_BUCKET /data
 
-/filebrowser --root=/data --address=0.0.0.0 --database=/config/filebrowser.db
+/filebrowser --root=/data --address=0.0.0.0 --database=/config/filebrowser.db $additional_flag
